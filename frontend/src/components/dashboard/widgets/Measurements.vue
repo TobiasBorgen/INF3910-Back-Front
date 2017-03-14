304cc7d50ac9ba1878ebe7527e4a44bd4892b0f9
Pwntus <pontus.edvard@gmail.com>
2017-03-14 18:14:03 +0100

    Stylings

--- frontend/src/components/dashboard/widgets/Measurements.vue
+++ frontend/src/components/dashboard/widgets/Measurements.vue
@@ -27,7 +27,7 @@ export default {
 <style lang="scss" scoped>
 .widget-measurements {
 	.md-card-content {
-		padding-top: 100px;
+		padding-top: 100px !important;
 		text-align: center;
 		font-weight: 300;
 		
