76f5704259797a9dd1e5495a96acfefac2bf7c2f
Pwntus <pontus.edvard@gmail.com>
2017-03-21 15:29:17 +0100

    Add MQTT client class. Logging.

--- server/src/lib/CloudConnect.js
+++ server/src/lib/CloudConnect.js
@@ -1,5 +1,6 @@
+const logger = require('../logger')
 const AWS = require('aws-sdk')
-const request = require("request");
+const request = require('request')
 
 class CloudConnect {
 
@@ -7,10 +8,14 @@ class CloudConnect {
 	 * Telenor Connexion/Cloud Connect/Start IoT.
 	 */
 	constructor () {
-		this.host = 'startiot.cc.telenorconnexion.com';
-		this.AWS = AWS;
-		this.manifest = null;
-		this.account = null;
+		this.host = 'startiot.cc.telenorconnexion.com'
+		this.AWS = AWS
+		this.manifest = null
+		this.account = null
+	}
+	
+	addSpinner (spinner) {
+		this.spinner = spinner
 	}
 
 	/* Load AWS manifest */
@@ -19,7 +24,7 @@ class CloudConnect {
 			.then(()=> {
 				return this.getCredentials()
 			})
-			.catch(error => console.log('Could not initialize CloudConnect:', error))
+			.catch(error => logger.error('-- CC: error,', error))
 	}
 
 	/* Parse different formats returned by a lambda call */
@@ -38,17 +43,17 @@ class CloudConnect {
 
 	/* Fetch manifest from correct URL */
 	loadManifest () {
-		const manifest_url = `https://1u31fuekv5.execute-api.eu-west-1.amazonaws.com/prod/manifest/?hostname=${this.host}`;
-		console.log('Start loadManifest')
+		const manifest_url = `https://1u31fuekv5.execute-api.eu-west-1.amazonaws.com/prod/manifest/?hostname=${this.host}`
+		
 		return new Promise((resolve, reject) => {
 				request(manifest_url, (error, response, body) => {
 					if (error)
 						reject(error)
 
 					const manifest = JSON.parse(body)
-					this.manifest = manifest;
-					this.AWS.config.region = manifest.Region;
-					console.log('End loadManifest')
+					this.manifest = manifest
+					this.AWS.config.region = manifest.Region
+					
 					resolve()
 				})
 		})
@@ -56,13 +61,12 @@ class CloudConnect {
 
 	/* Invoke will execute a AWS lambda function */
 	invoke (function_name, payload) {
-		console.log('Start of invoke')
-
+		
 		/* Create an instance of the lambda call for
 		 * potentially later usage.
 		 */
 		const invoke_instance = () => {
-			return this.lambda(function_name, payload);
+			return this.lambda(function_name, payload)
 		}
 
 		/* Run it, but catch errors */
@@ -87,10 +91,10 @@ class CloudConnect {
 			let params = {
 				FunctionName: this.manifest[function_name],
 				Payload: JSON.stringify(payload)
-			};
-			console.log('Params: -- ', params)
+			}
+			
 			/* Invoke the lambda function */
-			let lambda = new this.AWS.Lambda();
+			let lambda = new this.AWS.Lambda()
 			lambda.invoke(params, (err, res) => {
 
 				/* Parse response */
@@ -100,15 +104,15 @@ class CloudConnect {
 					/* Empty response */
 					if (!res || !res.Payload) reject('No data')
 					/* No error, got a response */
-					const payload = JSON.parse(res.Payload);
+					const payload = JSON.parse(res.Payload)
 					/* Got an error message in response */
 					if (res.FunctionError || payload.errorMessage)
 						reject(this.parseError(payload))
 					/* OK */
-					resolve(payload);
+					resolve(payload)
 				/* Unexpected error */
 				} catch (err) {
-					reject(err);
+					reject(err)
 				}
 			})
 		})
@@ -116,58 +120,55 @@ class CloudConnect {
 
 	/* Get AWS Cognito Credentials */
 	getCredentials (token = null) {
-		/* Don't fetch credentials if we already have them */
-		//if (token == null && this.AWS.config.credentials !== null) return Promise.resolve()
-
+		this.spinner.text = token ? 'Preparing Final Form' : 'Switching sides'
+		
 		this.AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 			IdentityPoolId: this.manifest.IdentityPool,
 			Logins: {
 				[`cognito-idp.${this.manifest.Region}.amazonaws.com/${this.manifest.UserPool}`]: token
 			}
-		});
+		})
 
 		/* Clear previously cached ID if token is absent */
 		if (!token) this.AWS.config.credentials.clearCachedId()
 
-		return this.AWS.config.credentials.getPromise();
+		return this.AWS.config.credentials.getPromise()
 	}
 
 	refreshCredentials () {
 		const account = this.account
-		console.log('Refreshaccount : -- ', account)
 
 		if (typeof account === 'undefined')
 			throw new Error('No Refresh Token')
 
-		const refreshToken = account.credentials.refreshToken;
+		const refreshToken = account.credentials.refreshToken
 
 		if (!refreshToken)
-			throw new Error('No Refresh Token');
+			throw new Error('No Refresh Token')
 
 		this.AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 			IdentityPoolId: this.manifest.IdentityPool
 		})
-		this.AWS.config.credentials.clearCachedId();
+		this.AWS.config.credentials.clearCachedId()
 
-		let accountData = null;
+		let accountData = null
 		const refreshPayload = {
 			action: 'REFRESH',
 			attributes: {
 				refreshToken: refreshToken
 			}
-		};
+		}
 		return this.invoke('AuthLambda', refreshPayload)
 			.then(account => {
-				this.account = account;
-				return this.getCredentials(account.credentials.token);
+				this.account = account
+				return this.getCredentials(account.credentials.token)
 			})
 			.then(() => { return Promise.resolve(this.account) })
 	}
 
 	/* Perform steps needed to create a Cognito Identity */
 	login (username, password) {
-		console.log('Startof login')
-
+		
 		/* Invoke an AuthLambda call to obtain an
 		 * authentication token from Cloud Connect.
 		 */
@@ -181,14 +182,15 @@ class CloudConnect {
 		return this.invoke('AuthLambda', loginPayload)
 			.then(account => {
 				this.account = account
-				console.log('Account:', account)
+				this.spinner.text = 'Dispatching carrier pigeons'
+				
 				/* Get AWS Cognito raised privilege credential
 				 * using the obtained Cloud Connect auth token.
 				 */
-				return this.getCredentials(account.credentials.token);
+				return this.getCredentials(account.credentials.token)
 			})
 			.then(() => { return Promise.resolve(this.account) })
 	}
 }
 
-module.exports = CloudConnect
+module.exports = new CloudConnect
---
+++ server/src/lib/MQTTClient.js
@@ -0,0 +1,28 @@
+const AWSIot = require('aws-iot-device-sdk')
+
+class MQTTClient {
+	constructor () {
+		this.client = null
+	}
+	
+	init (config) {
+		this.client = AWSIot.device({
+			region:					config.region,
+			accessKeyId:			config.credentials.accessKeyId,
+			secretKey:				config.credentials.secretAccessKey,
+			sessionToken:			config.credentials.sessionToken,
+			maximumReconnectTimeMs:	8000,
+			protocol: 'wss',
+		})
+	}
+	
+	updateWebsocketCredentials (config) {
+		this.client.updateWebsocketCredentials(
+			CC.AWS.config.credentials.accessKeyId,
+			CC.AWS.config.credentials.secretAccessKey,
+			CC.AWS.config.credentials.sessionToken
+		)
+	}
+}
+
+module.exports = new MQTTClient
