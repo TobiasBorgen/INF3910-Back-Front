c98b972f66c137914c5dc0b05bd5d4dbeb4ad820
Pwntus <pontus.edvard@gmail.com>
2017-03-21 15:29:17 +0100

    Add MQTT client class. Logging.

---
+++ server/src/backup_server.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+/* Input argv */
+var argv = require('optimist')
+	.demand('t').string('t')
+	.describe('t', 'Thing type ID')
+	.argv
+
+/* Libs */
+const mqtt = require('./lib/mqttClient')
+const logger = require('./logger')
+const path = require('path')
+const io = require('socket.io')()
+
+/* Init last data to send new socket clients */
+LAST_DATA = null
+
+/* Init Socket.io */
+io.on('connection', (client) => {
+	client.emit('message', LAST_DATA)	
+})
+io.listen(3000)
+
+/* Init MQTT client */
+const TOPIC = `$aws/things/${argv.t}/shadow/update`
+const options = {
+	hostname:	'a3k7odshaiipe8.iot.eu-west-1.amazonaws.com',
+	clientId:	argv.t,
+	key:		path.join(__dirname, '../certs', argv.t, 'privkey.pem'),
+	cert:		path.join(__dirname, '../certs', argv.t, 'cert.pem'),
+	ca:			path.join(__dirname, '../certs', 'root.crt')
+}
+const mqttClient = mqtt.init(options)
+
+/* Setup MQTT event listeners & connect */
+mqttClient.on('reconnect',	() => logger.warn('-- MQTT: reconnect'))
+mqttClient.on('close',		() => logger.warn('-- MQTT: connection closed'))
+mqttClient.on('error',		(e) => logger.error('-- MQTT: error, ', e))
+mqttClient.on('connect',	() => {
+	logger.info('-- MQTT: connected')
+	logger.info('-- MQTT: subscribing to ', TOPIC)
+
+	/* Subscribe to thing updates */
+	mqttClient.subscribe(TOPIC, {qos: 1}, (err, granted) => {
+		if (err) {
+			logger.error('-- MQTT: subscribe error, ', err)
+			return
+		}
+
+		/* Relay incoming messages over socket */
+		mqttClient.on('message', (topic, message) => {
+			const data = JSON.parse(message)
+			LAST_DATA = {thing: argv.t, data}
+
+			logger.info('-- MQTT: got message, ', data)
+			io.emit('message', LAST_DATA)
+		})
+	})
+})
+
+
+[
+	{
+		id: '00000376',
+		buffer: [
+			{
+				time: '123123123123',
+				s: '123213',
+				t: '1231',
+				d: '42342'
+			},
+			{
+				time: '123123123123',
+				s: '123213',
+				t: '1231',
+				d: '42342'
+			},
+		]
+	},
+	{
+		id: '00000374',
+		buffer: [
+			{
+				time: '123123123123',
+				s: '123213',
+				t: '1231',
+				d: '42342'
+			},
+			{
+				time: '123123123123',
+				s: '123213',
+				t: '1231',
+				d: '42342'
+			},
+		]
+	}
+]
--- server/src/server.js
+++ server/src/server.js
@@ -1,53 +1,66 @@
 #!/usr/bin/env node
+console.log('\033[2J')
 
 /* Input argv */
 var argv = require('optimist')
-    .demand('u').string('u')
-    .demand('p').string('p')
-    .argv
+	.demand('u').string('u')
+	.demand('p').string('p')
+	.argv
 
 /* Libs */
-const AWSIot = require('aws-iot-device-sdk')
-const CloudConnect = require('./lib/CloudConnect')
-let CC = new CloudConnect
+const ora = require('ora')
+const logger = require('./logger')
+const CC = require('./lib/CloudConnect')
+const MQTT = require('./lib/MQTTClient')
 
+/* Constants */
+const TOPIC = 'thing-update/UIT IFI course/vind/#'
+const spinner = ora('Pressing random buttons')
+spinner.color = 'green'
+CC.addSpinner(spinner.start())
 
+/* Init CC, fetch manifest */
 CC.init().then(() => {
-    CC.login(argv.u, argv.p).then(() => {
-
-        const mqtt = AWSIot.device({
-            region:             CC.AWS.config.region,
-            accessKeyId:        CC.AWS.config.credentials.accessKeyId,
-            secretKey:          CC.AWS.config.credentials.secretAccessKey,
-            sessionToken:       CC.AWS.config.credentials.sessionToken,
-            maximumReconnectTimeMs: 8000,
-            protocol: 'wss',
-        })
-
-        mqtt.on('reconnect', () => {
-            CC.refreshCredentials()
-						.then(() => {
-							mqtt.updateWebsocketCredentials(CC.AWS.config.credentials.accessKeyId, CC.AWS.config.credentials.secretAccessKey, CC.AWS.config.credentials.sessionToken)
-						})
-        })
-
-        mqtt.on('connect',   ()    => {
-					const topic = 'thing-update/UIT IFI course/vind/#'
-					mqtt.subscribe(topic, {qos: 1}, (err, granted) => {
-						if (err){
-							console.log(err)
-							return
-						}
-					})
-				})
-				mqtt.on('message', (topic, message) => {
-					console.log(topic)
-					console.log(JSON.parse(message.toString()))
-				})
-        mqtt.on('close',     ()    => console.info('MQTT Close'))
-        mqtt.on('error',     (err) => console.error('MQTT Error', err))
-    })
-		.catch(error => {
-			console.log('Server error: -- :', error)
-		})
+	/* Login using cmd parameters */
+	CC.login(argv.u, argv.p).then(() => {
+		
+		spinner.text = 'Docking spaceship'
+		
+		/* Init MQTT client with AWS config */
+		MQTT.init(CC.AWS.config)
+		
+		/* Setup event handlers */
+		MQTT.client.on('reconnect',	()					=> onReconnect())
+		MQTT.client.on('connect',	()					=> onConnect())
+		MQTT.client.on('message',	(topic, message)	=> onMessage(topic, message))
+		MQTT.client.on('close',		()					=> logger.warn('-- MQTT: connection closed'))
+		MQTT.client.on('error',		(e)					=> logger.error('-- MQTT: error,', e))
+	})
+	.catch(error => {
+		spinner.stop()
+		logger.error('-- CC: error,', error)
+	})
 })
+.catch(() => spinner.stop())
+
+/* Event handlers */
+const onReconnect = () => {
+	logger.warn('-- MQTT: reconnect')
+	CC.refreshCredentials().then(() => {
+		MQTT.updateWebsocketCredentials(CC.AWS.config)
+	})
+}
+
+const onConnect = () => {
+	spinner.stop()
+	logger.info('-- MQTT: connected')
+	logger.info('-- MQTT: subscribing to', TOPIC)
+	MQTT.client.subscribe(TOPIC, {qos: 1}, (err, granted) => {
+		if (err) logger.error('-- MQTT: error in message,', err)
+	})
+}
+
+const onMessage = (topic, message) => {
+	const data = JSON.parse(message)
+	logger.info(`-- MQTT: got message, [${topic}]\n`, data)
+}
